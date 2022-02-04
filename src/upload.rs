db2e7386934bcbb2843f77aa4f533e666d210d53
Jennings Zhang <jenni_zh@protonmail.com>
2022-02-04 01:02:22 -0500

    Combine files_under

--- src/upload.rs
+++ src/upload.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use crate::ChrisClient;
 
 
+/// Upload local files and directories to my ChRIS Library
 pub fn upload(client: &ChrisClient, files: &Vec<PathBuf>, path: &String) {
     let prefix = PathBuf::from(path);
     let all_files = discover_input_files(files);
@@ -14,37 +15,35 @@ pub fn upload(client: &ChrisClient, files: &Vec<PathBuf>, path: &String) {
 }
 
 
+/// Given a list of files and directories, traverse every directory
+/// to obtain just a list of files.
 fn discover_input_files(paths: &Vec<PathBuf>) -> Vec<PathBuf> {
     let mut all_files: Vec<PathBuf> = Vec::new();
     for path in paths {
-        let mut sub_files = files_under(path);
-        all_files.append(&mut sub_files)
+        match files_under(path) {
+            Ok(mut sub_files) => all_files.append(&mut sub_files),
+            Err(e) => panic!("{}", e)
+        }
     }
     all_files
 }
 
 
-fn files_under(path: &PathBuf) -> Vec<PathBuf> {
+/// Get all files under a path, whether the given path is a file or directory.
+fn files_under(path: &PathBuf) -> io::Result<Vec<PathBuf>> {
     if path.is_file() {
-        return vec![path.to_path_buf()]
+        return Ok(vec![path.to_path_buf()])
     }
-    match files_under_dir(path) {
-        Ok(sub_files) => sub_files,
-        Err(e) => panic!("{}", e)
-    }
-}
 
-
-fn files_under_dir(dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
     let mut sub_files: Vec<PathBuf> = Vec::new();
-    for entry in dir.read_dir()? {
+    for entry in path.read_dir()? {
         let entry = entry?;
         let sub_path = entry.path();
         if sub_path.is_file() {
             sub_files.push(sub_path)
         }
         else {
-            let mut nested_files = files_under_dir(&sub_path)?;
+            let mut nested_files = files_under(&sub_path)?;
             sub_files.append(&mut nested_files);
         }
     }
