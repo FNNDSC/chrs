2e5f9ec43c64496f842acc25dd81d94915596b2b
Jennings Zhang <Jennings.Zhang@childrens.harvard.edu>
2023-01-20 09:10:51 -0500

    Add module docs to lib.rs

--- chris/src/lib.rs
+++ chris/src/lib.rs
@@ -1,3 +1,6 @@
+//! Client library for [_ChRIS_](https://chrisproject.org/) built
+//! on [reqwest](https://crates.io/crates/reqwest).
+
 #[macro_use]
 extern crate shrinkwraprs;
 extern crate core;
